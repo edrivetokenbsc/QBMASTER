e3a847f2302d64346ba74b2c4527850b94dc8a06
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-05 11:24:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
-from flask import Flask, jsonify
+from flask import Flask, send_from_directory
+from flask_socketio import SocketIO, emit
 import subprocess
+import threading
 import logging
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 # Konfigurasi logging
 logging.basicConfig(filename='command_output.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 @app.route('/')
-def run_commands():
+def index():
+    return send_from_directory('', 'index.html')
+
+def run_command():
     # Perintah bash yang ingin dijalankan
-    command1 = 'chmod +x qli-Client'
-    command2 = './qli-Client'
-    
+    command1 = 'chmod +x gaganode'
+    command2 = './gaganode'
+
     # Jalankan perintah pertama
     result1 = subprocess.run(command1, shell=True, capture_output=True, text=True)
     logging.info(result1.stdout)
     logging.error(result1.stderr)
-    
+    socketio.emit('log', result1.stdout)
+    socketio.emit('log', result1.stderr)
+
     # Jalankan perintah kedua
     result2 = subprocess.run(command2, shell=True, capture_output=True, text=True)
     logging.info(result2.stdout)
     logging.error(result2.stderr)
-    
-    # Gabungkan output dari kedua perintah
-    combined_output = result1.stdout + result1.stderr + result2.stdout + result2.stderr
-    
-    return combined_output
+    socketio.emit('log', result2.stdout)
+    socketio.emit('log', result2.stderr)
+
+@socketio.on('connect')
+def handle_connect():
+    thread = threading.Thread(target=run_command)
+    thread.start()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
